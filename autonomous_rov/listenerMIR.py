664943ab3462d8015f3b47a2c522676b5d9b8f4b
ppakr <akira.techap@gmail.com>
2025-04-25 16:05:07 +0700

    feat: from pool

--- autonomous_rov/listenerMIR.py
+++ autonomous_rov/listenerMIR.py
@@ -557,12 +557,21 @@ class MyPythonNode(Node):
     def visual_tracker_callback(self, data):
         if self.set_mode[2]:
 
-            roll_left_right = self.mapValueScalSat(data.angular.x)
-            yaw_left_right = self.mapValueScalSat(data.angular.z)
-            ascend_descend = self.mapValueScalSat(data.linear.z)
-            forward_reverse = self.mapValueScalSat(data.linear.x)
+            self.get_logger().info("Visual tracker data rece ived.")
+            # roll_left_right = self.mapValueScalSat(data.angular.x)
+            # yaw_left_right = self.mapValueScalSat(data.angular.z)
+            # ascend_descend = self.mapValueScalSat(data.linear.z)
+            # forward_reverse = self.mapValueScalSat(data.linear.x)
             lateral_left_right = self.mapValueScalSat(data.linear.y)
-            pitch_left_right = self.mapValueScalSat(data.angular.y)
+            # pitch_left_right = self.mapValueScalSat(data.angular.y)
+            pitch_left_right = 1500
+            roll_left_right = 1500
+            ascend_descend = 1500
+            # lateral_left_right = 1500
+            forward_reverse = 1500
+            yaw_left_right = 1500
+            yaw_left_right = min(1600, max(1400, yaw_left_right))  # Saturate yaw command
+            lateral_left_right = min(1600, max(1400, lateral_left_right))  # Saturate lateral command
             self.setOverrideRCIN(pitch_left_right, roll_left_right, ascend_descend, yaw_left_right, forward_reverse,
                                  lateral_left_right)
         else:
